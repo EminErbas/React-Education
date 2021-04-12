196cbdc797846c8cc3f479ded0626352db8ef723
Muhammet Emin Erbaş <82402628+EminErbas@users.noreply.github.com>
2021-04-12 21:34:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+//import Hello from "./components/Hello";
+//import Greet from "./components/Greet";
+//import Welcome from './components/Welcome';
+//import Message from "./components/Message";
+//import Counter from "./components/Counter";
+//import ClassClick from "./components/ClassClick"
+//import FunctionClick from "./components/FunctionClick";
+//import ParentComponent from "./components/ParentComponent";
+import UserGreeting from './components/UserGreeting';
+//import EventBind from "./components/EventBind";
+function App() {
+  return (
+    <div className="App">
+      <UserGreeting />
+      {/*<ParentComponent />*/}
+      {/*<EventBind />*/}
+      {/*<FunctionClick />*/}
+      {/*<ClassClick />*/}
+      {/*<Counter />*/}
+      {/*<Greet name="Bruce" heroName="Batman">*/}
+        {/*}</div><p>This is children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Superman">
+        <button>Action</button>
+        </Greet>*/}
+      {/*<Greet name="Diana" heroName="Wonder Woman" />*/}
+      {/*<Welcome name="Bruce" heroName="Batman" />*/}
+      {/*<Welcome name="Clark" heroName="Superman" />*/}
+      {/*<Welcome name="Diana" heroName="Wonder Woman" />*/}
+      {/*<Hello />*/}
+      </div>
+  );
+}
+
+export default App;
